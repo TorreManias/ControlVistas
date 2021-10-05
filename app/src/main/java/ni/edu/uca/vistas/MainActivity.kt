33c5c40fdde6b9fb9656d6ef7d9c47cabad0679a
MarcosLanuza <marcoslanuza.t@gmail.com>
2021-10-05 17:03:20 -0600

    Se generan imagenes al inicio de la app

--- app/src/main/java/ni/edu/uca/vistas/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/vistas/MainActivity.kt
@@ -6,9 +6,20 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private var SENTIDO_ACTUAL = "SENTIDO"
+
+    private val VISTA = "vista"
+    private val GUSTO = "gusto"
+    private val OLFATO = "olfato"
+    private val OIDO = "oido"
+    private val TACTO = "tacto"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        generarNumeroAleatorio()
 
         val botonOtroSentido : Button = findViewById(R.id.btnOtro)
         botonOtroSentido.setOnClickListener{
@@ -33,6 +44,14 @@ class MainActivity : AppCompatActivity() {
             3 -> imagenSentido.setImageResource(R.drawable.img3)
             4 -> imagenSentido.setImageResource(R.drawable.img4)
             5 -> imagenSentido.setImageResource(R.drawable.img5)
+        }
+
+        when (nAleatorio) {
+            1 -> SENTIDO_ACTUAL = OIDO
+            2 -> SENTIDO_ACTUAL = VISTA
+            3 -> SENTIDO_ACTUAL = GUSTO
+            4 -> SENTIDO_ACTUAL = OLFATO
+            5 -> SENTIDO_ACTUAL = TACTO
         }
     }
 
